02ff377943f7e72ac56f52fffaf859e4adf038df
Sean McLinden <smclinden@users.noreply.github.com>
2023-09-30 15:28:48 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "SMCL_PETCL_RG"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "SMCL_PETCL_AN"
 }
 
 variable "location" {
